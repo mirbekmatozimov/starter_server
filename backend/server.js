b79e6fc03b556a283d9881862e1e9c3d603d592e
mirbek <mirbek.25.52@gmail.com>
2021-08-02 05:36:02 +0600

    mongoose connect

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); //thing that helps us to connect to mongodb
 
 require('dotenv').config(); //it return some object and than we run this method
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 //this middleware thing is to kind of run some plugins each time server runs
 app.use(cors());
 app.use(express.json()); //it is an analogy to bodyParser
+
+//connection to mongodb with mongoose
+const uri = process.env.ATLAS_URI; //saving data in dotenv and then set it to var
+mongoose.connect(uri, {useNewUrlParser: true,useCreateIndex: true, useUnifiedTopology: false});
+const connection = mongoose.connection;
+connection.once('open', ()=>{
+	console.log('MongoDB connection established successfully');
+})
+//end connection
 
 app.listen(port, ()=>{
 	console.log('Server is running on port: ${port}');
