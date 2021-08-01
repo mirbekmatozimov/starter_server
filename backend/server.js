67ae1e22ef3be7be3849a4b2ad2fab02eaec3095
mirbek <mirbek.25.52@gmail.com>
2021-08-02 04:35:47 +0600

    starterr files

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config(); //it return some object and than we run this method
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+//this middleware thing is to kind of run some plugins each time server runs
+app.use(cors());
+app.use(express.json()); //it is an analogy to bodyParser
+
+app.listen(port, ()=>{
+	console.log('Server is running on port: ${port}');
+});
+
+
